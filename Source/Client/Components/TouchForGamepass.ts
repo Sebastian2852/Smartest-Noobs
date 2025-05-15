67004f4685b72ba4e0a6719a5f5b5611d2cdf9ce
Sebastian <sebastian2852@proton.me>
2025-05-15 13:30:40 +0100

    deleting touch for gamepass if gamepass is owned

--- Source/Client/Components/TouchForGamepass.ts
+++ Source/Client/Components/TouchForGamepass.ts
@@ -1,6 +1,6 @@
 import { BaseComponent, Component } from "@flamework/components";
 import { OnStart } from "@flamework/core";
-import { MarketplaceService, Players } from "@rbxts/services";
+import { MarketplaceService, Players, RunService } from "@rbxts/services";
 
 interface TouchForGamepassAttributes {
 	gamepassId: number;
@@ -12,6 +12,12 @@ interface TouchForGamepassAttributes {
 export default class TouchForGamepass extends BaseComponent<TouchForGamepassAttributes, BasePart> implements OnStart {
 	onStart() {
 		const part = this.instance;
+
+		const player = Players.LocalPlayer;
+		if (MarketplaceService.UserOwnsGamePassAsync(player.UserId, this.attributes.gamepassId)) {
+			this.destroy();
+		}
+
 		part.Touched.Connect((other) => {
 			if (other.FindFirstAncestorWhichIsA("Model")?.FindFirstChildWhichIsA("Humanoid") === undefined) return;
 			const character = other.FindFirstAncestorWhichIsA("Model");
